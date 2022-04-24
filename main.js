f731abbc28036260fb9e11d855e045faa4dfc3a7
Johannes Moe <jojoomoe@protonmail.com>
2022-04-24 03:26:00 +0200

    inital commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let sketch = function(p) {
+  let x = 100;
+  let y = 100;
+
+  p.setup = function() {
+    p.createCanvas(windowWidth, windowHeight);
+  };
+
+  p.draw = function() {
+    p.background(0);
+    p.fill(255);
+    p.rect(x, y, 50, 50);
+  };
+
+
+};
+
+let myp5 = new p5(sketch);
